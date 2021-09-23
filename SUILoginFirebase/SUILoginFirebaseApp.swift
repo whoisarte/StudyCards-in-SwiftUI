a4b30307790928de7fcfc758a528cf6992e50ac1
Artemio Pánuco <arte.solo.arte@hotmail.com>
2021-09-23 12:25:55 -0500

    Initial Commit

---
+++ SUILoginFirebase/SUILoginFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUILoginFirebaseApp.swift
+//  SUILoginFirebase
+//
+//  Created by Artemio on 23/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SUILoginFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
